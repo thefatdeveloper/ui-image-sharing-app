5c316e51a00c00f4b45752fa0e6f6e4854b1d9f9
saisreekar17 <saisreekar17@gmail.com>
2024-08-10 06:23:40 +0000

    changed

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Profile from "./pages/profile/Profile";
 
+//changed
 function App() {
   const { user } = useSelector((state) => state.user);
 
